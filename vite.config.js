843de14117a355d4b8fcfdd6a3b4bd09b652a473
ysaaba <nickschack@gmail.com>
2024-12-14 17:51:24 +0100

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: './',
-  // ... rest of your config
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 }) 
