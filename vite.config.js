86507118b5011c6afd36700c4863706fafd8d74e
ysaaba <nickschack@gmail.com>
2024-12-14 18:17:17 +0100

    Update Vite configuration for local development and adjust asset output paths
    - Change base path in vite.config.js from '/Chordie/' to './' for local testing.
    - Update asset file naming conventions in Rollup options to include hashes for better cache management.
    - Remove unused CSS and JS files from the dist/assets directory to clean up the build output.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Chordie/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
       }
     }
   }
